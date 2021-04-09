60858d7fed248e8a9988025348ec7fe124fdf49e
AnsariKirdar <33788641+AnsariKirdar@users.noreply.github.com>
2021-04-09 18:03:50 +0530

    a

--- index.php
+++ index.php
@@ -17,24 +17,24 @@
   <meta name="author" content="Vision technology">
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website">
-  <meta property="og:url" content="https://metatags.io/">
+  <meta property="og:url" content="<?php echo  $BASE_URL ?>/">
   <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
   <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-  <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+  <meta property="og:image" content="<?php echo  $BASE_URL ?>/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
   <meta property="profile:first_name" content="Kirdar">
   <meta property="profile:last_name" content="Ansari">
   <meta property="business:contact_data:country_name" content="India">
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image">
-  <meta property="twitter:url" content="https://metatags.io/">
+  <meta property="twitter:url" content="<?php echo  $BASE_URL ?>/">
   <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
   <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-  <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+  <meta property="twitter:image" content="<?php echo  $BASE_URL ?>/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 
 
   <link rel="manifest" href="visiontechnology.webmanifest">
-  <link rel="shortcut icon" type="image/x-icon" href="./img/logo/favicon.ico">
-  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo  $BASE_URL ?>/img/logo/favicon.ico">
+  <link href="<?php echo  $BASE_URL ?>/css/bootstrap.min.css" rel="stylesheet">
 
   <title>Vision Technology</title>
 </head>
@@ -43,10 +43,10 @@
   <?php include $BASE_URL . 'inc/nav.php'; ?>
   <h1>Hello, world!</h1>
 
-  <?php include './inc/footer.php'; ?>
-  <script src="./js/bootstrap.bundle.min.js"></script>
-  <script src="./js/fontawsome.js"></script>
-  <script src="./js/main.js"></script>
+  <?php include $BASE_URL . 'inc/footer.php'; ?>
+  <script src="<?php echo  $BASE_URL ?>/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo  $BASE_URL ?>/js/fontawsome.js"></script>
+  <script src="<?php echo  $BASE_URL ?>/js/main.js"></script>
 </body>
 
 </html>
