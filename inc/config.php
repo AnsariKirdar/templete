9d74536a9f1517b96ab8293a7136f9cc1e8bd2ea
Kirdar <33788641+AnsariKirdar@users.noreply.github.com>
2022-12-28 05:03:23 +0530

    Base URL Get Funtion Update

--- inc/config.php
+++ inc/config.php
@@ -3,7 +3,14 @@ $TITLE = "Vision Technology is website Designing Development agency in Mumbai ";
 $META_DESCRIPTION = "Vision Technology, the best creative agency in Mumbai For effective marketing and design solutions. Boost business with our digital marketing Plans. ";
 $META_DESCRIPTION_KEY = "digital marketing, website designing, website development, management erp, erp and software";
 $OG_TYPE = "website";
-$BASE_URL = "http://localhost/templete";
+
+$BASE_URLs = $_SERVER['REMOTE_ADDR'];
+if ($BASE_URLs == '::1' || $BASE_URLs == '127:0:0:1') {
+  $BASE_URL = 'http://localhost/templete';
+}else{
+  $BASE_URL = "";
+}
+
 $OG_IMAGE = "$BASE_URL/img/logo/logo.png";
 $OG_IMAGE_WIDTH = "200";
 $OG_IMAGE_HEIGHT = "200";
